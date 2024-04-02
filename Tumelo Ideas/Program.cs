d7e399907dab6265969d1a5653c830e2da45002a
Mukelani Guma Mashoba <mukelanilastborn@gmail.com>
2024-04-02 19:40:59 +0200

    adding a lop to re use the program

--- Tumelo Ideas/Program.cs
+++ Tumelo Ideas/Program.cs
@@ -9,44 +9,61 @@ namespace Tumelo_Ideas
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Which side you looking for ? enter r or x or y;");
-            char side = char.Parse(Console.ReadLine());
 
-            double a, h, op;
-            Console.WriteLine("'|\\\'");
-            Console.WriteLine("'|\\\\\'");
-            Console.WriteLine("'|\\\\\\'");
+            bool check = true;
 
-            double answer = 0;
-            _ = Math.Pow(answer, 2);
+            while (check != false) {
+                Console.Write("Enter CAL to calculate any side ? using theorem of pythagoras :");
+                string play = Console.ReadLine().ToUpper();
 
-            // r^2 = x^2 + y^2
-            // r HYpotenus is x adjecent plus opposite which is y
+                if (play == "CAL")
+                {
+                    Console.WriteLine("Which side you looking for ? enter r or x or y;");
+                    char side = char.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter given side Adjacent side : ");
-            a = double.Parse(Console.ReadLine());
-            Console.WriteLine("Enter given side Hypoternus : ");
-            h = double.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the Opposite Side");
-            op = double.Parse(Console.ReadLine());
+                    double a, h, op;
+                    Console.WriteLine("'|\\\'");
+                    Console.WriteLine("'|\\\\\'");
+                    Console.WriteLine("'|\\\\\\'");
 
-            if (side == 'r')
-            {
-                answer = Math.Pow(a, 2) + Math.Pow(op, 2);
-                double hypotonus = Math.Sqrt(answer);
-                Console.WriteLine($"The answer for side {side} and the answer is {hypotonus}");
-            }
-            else if (side == 'x')
-            {
-                answer = Math.Pow(h, 2) - Math.Pow(op, 2);
-                double adjacent = Math.Sqrt(answer);
-                Console.WriteLine($"The answer for side {side} and the answer is {adjacent}");
-            }
-            else if (side == 'y')
-            {
-                answer = Math.Pow(h, 2) - Math.Pow(a, 2);
-                double opposite = Math.Sqrt(answer);
-                Console.WriteLine($"The answer for side {side} and the answer is {opposite}");
+                    double answer = 0;
+                    _ = Math.Pow(answer, 2);
+
+                    // r^2 = x^2 + y^2
+                    // r HYpotenus is x adjecent plus opposite which is y
+
+                    Console.WriteLine("Enter given side Adjacent side : ");
+                    a = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter given side Hypoternus : ");
+                    h = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the Opposite Side");
+                    op = double.Parse(Console.ReadLine());
+
+                    if (side == 'r')
+                    {
+                        answer = Math.Pow(a, 2) + Math.Pow(op, 2);
+                        double hypotonus = Math.Sqrt(answer);
+                        Console.WriteLine($"The answer for side {side} and the answer is {hypotonus}");
+                    }
+                    else if (side == 'x')
+                    {
+                        answer = Math.Pow(h, 2) - Math.Pow(op, 2);
+                        double adjacent = Math.Sqrt(answer);
+                        Console.WriteLine($"The answer for side {side} and the answer is {adjacent}");
+                    }
+                    else if (side == 'y')
+                    {
+                        answer = Math.Pow(h, 2) - Math.Pow(a, 2);
+                        double opposite = Math.Sqrt(answer);
+                        Console.WriteLine($"The answer for side {side} and the answer is {opposite}");
+                    }
+                }
+                else
+                {
+                    Console.Write("Good Bye Thanks for using the program....");
+                    check = false;
+                    break;
+                }
             }
             _ = Console.ReadKey();
 
